d8d1bb109c4bb265dac77ff275b9c81200bfffad
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-11-03 21:07:32 +0000

    Handling DB errors with meaningful messages

---
+++ server/helpers/dbErrorHandler.js
@@ -0,0 +1,33 @@
+/* DB Error Handling */
+
+const getErrorMessage = (error) => {
+    let message = ''
+    if (error.code) {
+        switch (error.code) {
+            case 11000:
+            case 11001:
+                message = getUniqueErrorMessage(error)
+                break
+            default:
+                message = 'Something went wrong'
+        }
+    } else {
+        for (let errName in error.errors) {
+            if (error.errors[errName].message) message = error.errors[errName].message
+        }
+    }
+    return message
+}
+
+export default {getErrorMessage}
+
+const getUniqueErrorMessage = (error) => {
+    let output
+    try {
+        let fieldName = error.message.substring(error.message.lastIndexOf('.$') + 2, error.message.lastIndexOf('_1'))
+        output = fieldName.charAt(0).toLocaleLowerCase() + fieldName.slice(1) + ' already exists'
+    } catch (e) {
+        output = 'Unique field already exists'
+    }
+    return output
+}
