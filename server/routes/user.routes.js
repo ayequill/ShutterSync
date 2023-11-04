01c1ebe22ac19b258a98a8206f8b33201d76aa4c
Siaw A. Nicholas <54719123+ayequill@users.noreply.github.com>
2023-11-05 01:49:03 +0700

    Setup client boilerplate and CI/CD (#3)
    * refactor(.gitignore): updated ignored list
    
    * style: reformatted files
    
    * build(package.json): added dependencies
    
    * feat(frontend): added boilerplate for client-side
    
    * build: added config files
    
    * build: added jest config
    
    * build: added ci script
    
    * refactor: removed backend testing
    
    * build: added lock files
    
    * fix: refactored
    
    * ci: reafctored
    
    * ci: added lockfile
    
    * fix: fix ci
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix: fix
    
    * fix(fix): fix

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -1,16 +1,16 @@
+import express from 'express';
 import userCtrl from '../controllers/user.controller.js';
-import userAuth from '../controllers/auth.controller.js'
-import express from "express";
+import userAuth from '../controllers/auth.controller.js';
 
-const router = express.Router()
+const router = express.Router();
 
-router.route('/api/users').post(userCtrl.create)
-router.route('/api/users').get(userCtrl.list)
-router.route('/api/users/:userId')
-    .get(userAuth.requireSignIn, userCtrl.read)
-    .put(userAuth.requireSignIn,userAuth.isAuthorized, userCtrl.update)
-    .delete(userAuth.requireSignIn,userAuth.isAuthorized, userCtrl.remove)
-router.param('userId', userCtrl.userByID)
+router.route('/api/users').post(userCtrl.create);
+router.route('/api/users').get(userCtrl.list);
+router
+  .route('/api/users/:userId')
+  .get(userAuth.requireSignIn, userCtrl.read)
+  .put(userAuth.requireSignIn, userAuth.isAuthorized, userCtrl.update)
+  .delete(userAuth.requireSignIn, userAuth.isAuthorized, userCtrl.remove);
+router.param('userId', userCtrl.userByID);
 
-
-export default router
+export default router;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,35 @@
-import app from "./app.js";
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
+import app from './app.js';
 
 dotenv.config();
 
-const DB_URI = process.env.MONGO_URI
-const PORT = process.env.PORT
+const { DB_URI, PORT } = process.env;
 
 // Connecting to Mongo using mongoose
-mongoose.connect(DB_URI, {
+mongoose
+  .connect(DB_URI, {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => {
-    console.log('Connected to MongoDB!')
-}).catch(err => {
-    console.log('Error connecting to MongoDB')
-    console.log(err)
-})
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.log('Error connecting to MongoDB');
+    console.log(err);
+  });
 
 // Close the db when node process stops
 process.on('SIGINT', () => {
-    mongoose.connection.close(() => {
-        process.exit(0)
-    }).then(() => {
-        console.log('Disconnected from MongoDB!')
+  mongoose.connection
+    .close(() => {
+      process.exit(0);
     })
-})
+    .then(() => {
+      console.log('Disconnected from MongoDB!');
+    });
+});
 app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`)
-})
+  console.log(`Server started on port ${PORT}`);
+});
