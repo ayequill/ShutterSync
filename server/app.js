d8cce2f860a8e9130f957334cceac409d388bb1c
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-11-11 14:41:30 +0000

    feat: multer storage

--- server/app.js
+++ server/app.js
@@ -5,9 +5,28 @@ import cors from 'cors';
 import helmet from 'helmet';
 import userRoutes from './routes/user.routes.js';
 import authRoutes from './routes/auth.routes.js';
-
+import multer from 'multer';
+import { v4 as uuidv4 } from 'uuid';
 // create express app
 const app = express();
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, 'public/images/');
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image/')) {
+    cb(null, true);
+  } else {
+    cb({ error: 'File type not supported' }, false);
+  }
+};
+export const upload = multer({ storage, fileFilter });
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
