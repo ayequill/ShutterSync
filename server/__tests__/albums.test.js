eb59c55352a015c33cbb8a828a041d395bce7308
Siaw A. Nicholas <siawnic.dev@gmail.com>
2023-11-29 00:24:20 +0000

    test: add verbose and collectCoverage options to jest.config.json, update test descriptions in albums.test.js, and fix getAlbum function in album.controller.js

--- server/__tests__/albums.test.js
+++ server/__tests__/albums.test.js
@@ -22,17 +22,17 @@ describe('Test Albums', () => {
     // await Album.deleteMany({});
   });
   afterAll(async () => {
-    // await Album.deleteMany({});
+    await Album.deleteMany({});
     await mongoose.connection.close();
   });
 
-  test('Get All Albums from user', async () => {
+  test('/GET Get All Albums from user', async () => {
     const res = await req.get(baseURL).set('x-api-key', API_KEY);
     expect(res.status).toBe(200);
     expect(res.body).toBeInstanceOf(Array);
   }, 10000);
 
-  test('Create an album', async () => {
+  test('/POST Create an album', async () => {
     const album = { name: 'Test Album' };
     const res = await req.post(baseURL).send(album).set('x-api-key', API_KEY);
 
@@ -50,6 +50,50 @@ describe('Test Albums', () => {
   test('/GET Get a single album with api key attached', async () => {
     const res = await req
       .get(`${baseURL}/${albumID}`)
+      .set('x-api-key', API_KEY);
+
+    expect(res.status).toBe(200);
+  });
+
+  test('/GET Get a single album with invalid id', async () => {
+    const res = await req.get(`${baseURL}/123`).set('x-api-key', API_KEY);
+
+    expect(res.status).toBe(500);
+  });
+
+  test('/GET Get a single album with valid id', async () => {
+    const res = await req
+      .get(`${baseURL}/${albumID}`)
+      .set('x-api-key', API_KEY);
+
+    expect(res.status).toBe(200);
+  });
+
+  test('/PUT Update a single album with invalid id', async () => {
+    const res = await req.put(`${baseURL}/123`).set('x-api-key', API_KEY);
+
+    expect(res.status).toBe(500);
+  });
+
+  test('/PUT Update a single album with valid id', async () => {
+    const res = await req
+      .put(`${baseURL}/${albumID}`)
+      .set('x-api-key', API_KEY)
+      .send({ name: 'Updated Album' });
+
+    expect(res.status).toBe(200);
+    expect(res.body.name).toBe('Updated Album');
+  });
+
+  test('/DELETE Delete a single album with invalid id', async () => {
+    const res = await req.delete(`${baseURL}/123`).set('x-api-key', API_KEY);
+
+    expect(res.status).toBe(500);
+  });
+
+  test('/DELETE Delete a single album with valid id', async () => {
+    const res = await req
+      .delete(`${baseURL}/${albumID}`)
       .set('x-api-key', API_KEY);
 
     expect(res.status).toBe(200);
